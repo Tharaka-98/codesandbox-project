b25a90ff649c8705266905bbad4331dfbb185232
Tharaka Senevirathne <128624191+Tharaka-98@users.noreply.github.com>
2023-04-19 00:44:52 +0530

    Initial commit

---
+++ src/components/Notes.jsx
@@ -0,0 +1,15 @@
+const react = require("react");
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>react.js Bootcamp</h1>
+      <p>
+        we learnt the basics of Web-Development, Javascript, JSX, React,
+        ReactDOM and Created a project at the very end of the bootcamp as well.
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
